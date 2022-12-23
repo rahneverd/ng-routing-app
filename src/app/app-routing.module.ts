6b19a57053aa3201285e7e608c2dddfefcb8efc1
rahneverd <rahneverd@gmail.com>
2022-12-23 16:51:50 +0500

    WildCard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'departments', component: DepartmentListComponent},
-  {path: 'employees', component: EmployeeListComponent}
+  {path: 'employees', component: EmployeeListComponent},
+  {path: "**", component: PageNotFoundComponent}
 ];
 
 @NgModule({
