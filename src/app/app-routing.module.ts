6a3f9f4c42281de6d13174a7cdd676c336fc8040
rahneverd <rahneverd@gmail.com>
2022-12-23 15:42:40 +0500

    bootstrap added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 const routes: Routes = [
   {path: 'departments', component: DepartmentListComponent},
-  {path: 'employees', component: DepartmentListComponent}
+  {path: 'employees', component: EmployeeListComponent}
 ];
 
 @NgModule({
