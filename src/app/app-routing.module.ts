4c21fa5598a9112a37c3ab4c13f6d34ac6cc3947
rahneverd <rahneverd@gmail.com>
2022-12-23 15:02:45 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'departments', component: DepartmentListComponent},
+  {path: 'employees', component: DepartmentListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
